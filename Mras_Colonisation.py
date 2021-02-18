86501f04285dfb5914b2506b60a26d9fbcd4817a
Dingovina <65973711+Dingovina@users.noreply.github.com>
2021-02-18 22:08:40 +0500

    Результат отбора + css change

--- Mras_Colonisation.py
+++ Mras_Colonisation.py
@@ -253,5 +253,37 @@ def choice(planet):
                         </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static',
+                                                                                  filename='css/style.css')}" />
+                            <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                            <title>Результаты</title>
+                          </head>
+                          <body>
+                            <h2>Результаты отбора</h2>
+                            <h4>Претендента на участие в миссии {nickname}:</h4>
+                            <div class="alert alert-success" role="alert">
+                                Поздравляем! Вас рейтинг после {level} этапа отбора
+                            </div>
+                            <div class="alert alert-success" role="alert" id="uncolored">
+                                составляет {rating}
+                            </div>
+                            <div class="alert alert-warning" role="alert">
+                                Желаем удачи!
+                            </div>
+
+
+                          </body>
+                        </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
