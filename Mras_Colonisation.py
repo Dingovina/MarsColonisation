13c1d576b01cda143c0d656811f694f5a76f0a98
Dingovina <65973711+Dingovina@users.noreply.github.com>
2021-02-18 19:36:32 +0500

    Рекламная компания + static

---
+++ Mras_Colonisation.py
@@ -0,0 +1,34 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def empty():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def promotion():
+    s = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+         'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                        {'<br>'.join(s)}
+                  </body>
+                </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
